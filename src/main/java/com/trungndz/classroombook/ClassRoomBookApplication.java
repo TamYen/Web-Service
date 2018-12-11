5e2ddc1166300438407251185d5b699fbf1505f0
TamYen <34823904+TamYen@users.noreply.github.com>
2018-12-11 23:13:57 +0700

    web seervice

---
+++ src/main/java/com/trungndz/classroombook/ClassRoomBookApplication.java
@@ -0,0 +1,12 @@
+package com.trungndz.classroombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRoomBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRoomBookApplication.class, args);
+	}
+}
