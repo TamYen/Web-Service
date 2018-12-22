e93cf0fba9bade2f3fe7202d8269264fe9f9a3cb
TamYen <34823904+TamYen@users.noreply.github.com>
2018-12-23 01:24:13 +0700

    @controller

--- src/main/java/com/trungndz/classroombook/controller/RoomsessionController.java
+++ src/main/java/com/trungndz/classroombook/controller/RoomsessionController.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.Controller;
 
 import com.trungndz.classroombook.entities.Employee;
 import com.trungndz.classroombook.entities.Position;
